61291eb26c409ba79480d86af0f7ea0f5331ab2b
takayoshi.uchida <taum0715@gmail.com>
2015-11-23 17:07:41 +0900

    first commit

---
+++ MvcMovie/src/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.WebEncoders;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            //return "This is my default action...";
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            //return HtmlEncoder.Default.HtmlEncode("Hello " + name + ", ID is: " + ID);
+
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ MvcMovie/src/MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,150 @@
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+using MvcMovie.Models;
+using Microsoft.AspNet.Http.Internal;
+using System.Collections.Generic;
+using Microsoft.AspNet.Mvc.Rendering;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Movies
+        public IActionResult Index(string genreList, string searchString)
+        {
+            var GenreQry = from m in _context.Movie
+                           orderby m.Genre
+                           select m.Genre;
+
+            var GenreList = new List<string>();
+            GenreList.AddRange(GenreQry.Distinct());
+            ViewData["GenreList"] = new SelectList(GenreList);
+
+            //var movies = _context.Movie.Select(m => m);
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(genreList))
+            {
+                movies = movies.Where(x => x.Genre == genreList);
+            }
+
+            return View(movies);
+        }
+
+        // POST: Movies
+        [HttpPost]
+        public string Index(FormCollection fc, string searchString)
+        {
+            return "From [HttpPost] Index: filter on " + searchString;
+        }
+
+        // GET: Movies/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Movie movie = _context.Movie.Single(m => m.ID == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Movie.Add(movie);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Movie movie = _context.Movie.Single(m => m.ID == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([Bind("ID,Title,ReleaseDate,Genre,Price,Rating")]Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(movie);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Delete/5
+        [ActionName("Delete")]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Movie movie = _context.Movie.Single(m => m.ID == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Movie movie = _context.Movie.Single(m => m.ID == id);
+            _context.Movie.Remove(movie);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
