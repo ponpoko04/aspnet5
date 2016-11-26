c2a625001215e121de9e6fda15e272b042140877
takayoshi.uchida <taum0715@gmail.com>
2016-11-26 18:04:43 +0900

    ToDoApplication Sample Create

---
+++ ConsoleApplication1/ConsoleApplication1/Format.cs
@@ -0,0 +1,18 @@
+//using System;
+//namespace Solution
+//{
+//    class Solution
+//    {
+//        static void Main(string[] args)
+//        {
+//            var line1 = Console.ReadLine().Trim();
+//            var N = Int32.Parse(line1);
+//            for (var i = 0; i < N; i++)
+//            {
+//                var data = Console.ReadLine().Trim();
+//                var a = (data.Split(' ').Length > 0) ? Int32.Parse(data.Split(' ')[0]) : 1;
+//                Console.WriteLine("");
+//            }
+//        }
+//    }
+//}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+namespace Solution
+{
+    class Solution
+    {
+        enum GoToPosition
+        {
+            Up,
+            Down,
+            Right,
+            Left
+        }
+
+        static void Main(string[] args)
+        {
+            var line1 = Console.ReadLine().Trim();
+            var H = (line1.Split(' ').Length > 0) ? Int32.Parse(line1.Split(' ')[0]) : 1;
+            var W = (line1.Split(' ').Length > 1) ? Int32.Parse(line1.Split(' ')[1]) : 1;
+            var boxes = new char[H, W];
+            for (var i = 0; i < H; i++)
+            {
+                var data = Console.ReadLine().Trim().ToCharArray();
+                for (int j = 0; j < W; j++)
+                {
+                    boxes[i, j] = data[j];
+                }
+            }
+            var nowH = 0;
+            var nowW = 0;
+            var throughBoxes = 0;
+            var nowGoToPosition = GoToPosition.Right;
+            while (true)
+            {
+                if (nowH >= H || nowH < 0 || nowW >= W || nowW < 0) break;
+                var box = boxes[nowH, nowW];
+                throughBoxes += 1;
+                switch (box)
+                {
+                    case '_':
+                        switch(nowGoToPosition)
+                        {
+                            case GoToPosition.Up:
+                                nowH -= 1;
+                                break;
+                            case GoToPosition.Down:
+                                nowH += 1;
+                                break;
+                            case GoToPosition.Right:
+                                nowW += 1;
+                                break;
+                            case GoToPosition.Left:
+                                nowW -= 1;
+                                break;
+                        }
+                        break;
+
+                    case '\\':
+                        switch (nowGoToPosition)
+                        {
+                            case GoToPosition.Up:
+                                nowW -= 1;
+                                nowGoToPosition = GoToPosition.Left;
+                                break;
+                            case GoToPosition.Down:
+                                nowW += 1;
+                                nowGoToPosition = GoToPosition.Right;
+                                break;
+                            case GoToPosition.Right:
+                                nowH += 1;
+                                nowGoToPosition = GoToPosition.Down;
+                                break;
+                            case GoToPosition.Left:
+                                nowH -= 1;
+                                nowGoToPosition = GoToPosition.Up;
+                                break;
+                        }
+                        break;
+
+                    case '/':
+                        switch (nowGoToPosition)
+                        {
+                            case GoToPosition.Up:
+                                nowW += 1;
+                                nowGoToPosition = GoToPosition.Right;
+                                break;
+                            case GoToPosition.Down:
+                                nowW -= 1;
+                                nowGoToPosition = GoToPosition.Left;
+                                break;
+                            case GoToPosition.Right:
+                                nowH -= 1;
+                                nowGoToPosition = GoToPosition.Up;
+                                break;
+                            case GoToPosition.Left:
+                                nowH += 1;
+                                nowGoToPosition = GoToPosition.Down;
+                                break;
+                        }
+                        break;
+                }
+            }
+            Console.WriteLine(throughBoxes);
+        }
+    }
+}
